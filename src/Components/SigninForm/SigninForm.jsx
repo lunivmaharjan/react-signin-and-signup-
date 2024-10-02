3222f752c7d9c7b3775ac50bea8e892c655defbb
luni maharjan <lunivmaharjan33@gmail.com>
2024-10-02 18:16:34 +0545

    Sign in edit

--- src/Components/SigninForm/SigninForm.jsx
+++ src/Components/SigninForm/SigninForm.jsx
@@ -4,20 +4,24 @@ import '../../App.css'
 
 const SigninForm = () => {
   return (
-    <div className="wrapper">
-        <div action=''>
-            <h1>Signin Form</h1>
-        </div> 
-        <br></br>
-    <div className='input-box'>
-        <input type='text' placeholder='Email' required/>
-    </div>
-    <br></br>
-    <div className='input-box'>
-        <input type='password' placeholder='Password' required/>
-    </div>
-    <br></br>
-    <button type='Submit'>Submit</button>
+<div className='wrapper'>
+        <div action="">
+         <h1>Sign In</h1>
+         <div >
+         <div className="input-box">
+            <input type="text" placeholder='Email' className='input_form' required/>
+         </div>
+         <div className="input-box">
+            <input type="Password" placeholder='Password' className='input_form' required/>
+         </div>
+         </div>
+         <div className="remember-forgot">
+           <label><input type="checkbox"/>Remember me</label> 
+            <a href="#">Forget Password</a>
+         </div>
+
+         <button type="submit">Login</button>
+        </div>
     </div>
   )
 }
