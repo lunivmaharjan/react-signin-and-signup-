972d11592e7c3af8627d7aaa7278d79c4e3545ba
luni maharjan <lunivmaharjan33@gmail.com>
2024-10-02 12:48:48 +0545

    signin file

--- src/Components/SigninForm/SigninForm.jsx
+++ src/Components/SigninForm/SigninForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './SigninForm.css'
+import '../../App.css'
 
 const SigninForm = () => {
   return (
@@ -7,12 +8,15 @@ const SigninForm = () => {
         <div action=''>
             <h1>Signin Form</h1>
         </div> 
+        <br></br>
     <div className='input-box'>
         <input type='text' placeholder='Email' required/>
     </div>
+    <br></br>
     <div className='input-box'>
         <input type='password' placeholder='Password' required/>
     </div>
+    <br></br>
     <button type='Submit'>Submit</button>
     </div>
   )
