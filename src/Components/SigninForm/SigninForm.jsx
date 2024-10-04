67cbe13ae30abe4a204491bbc0e52dfe99451945
luni maharjan <lunivmaharjan33@gmail.com>
2024-10-04 15:34:17 +0545

    signin edit

--- src/Components/SigninForm/SigninForm.jsx
+++ src/Components/SigninForm/SigninForm.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import './SigninForm.css'
-import '../../App.css'
+import '../../index.css'
+import { MdOutlineMail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
+
 
 const SigninForm = () => {
+
   return (
 <div className='wrapper'>
         <div action="">
@@ -10,16 +13,18 @@ const SigninForm = () => {
          <div >
          <div className="input-box">
             <input type="text" placeholder='Email' className='input_form' required/>
+            <MdOutlineMail className="icon" />
          </div>
          <div className="input-box">
             <input type="Password" placeholder='Password' className='input_form' required/>
+            <RiLockPasswordLine className="icon" />
          </div>
          </div>
          <div className="remember-forgot">
            <label><input type="checkbox"/>Remember me</label> 
             <a href="#">Forget Password</a>
          </div>
-
+          <br></br>
          <button type="submit">Login</button>
         </div>
     </div>
